6b6fc16d589c480f26d8cfcaec3254035964102e
darelg2 <darelg2@uw.edu>
2022-04-13 22:36:24 -0700

    implement speed slider

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from asyncio.windows_events import NULL
 import tkinter as tk
 import numpy as np
 from PIL import Image, ImageTk
+from copy import deepcopy
 from maze import Maze, TYPES
 from constants import WALL, CELL, VISITED, STATE_TO_COLOR
 
@@ -15,6 +15,9 @@ class AnimationScreen(tk.Canvas):
         self.image_canvas = self.create_image(0,0,anchor=tk.NW,
             image=self.blank_img)
         self.is_animate = False
+        self.maze_manager = Maze(type="eller", width=10, height=10)
+        self.animation_state = AnimationState(self.maze_manager)
+        self.maze_scale = int(min(self.canvas_height/10, self.canvas_width/10))
 
     def update_screen(self, maze_type, maze_width, maze_height):
         self.maze_manager = Maze(maze_type, maze_width, maze_height)
@@ -40,15 +43,16 @@ class AnimationScreen(tk.Canvas):
         self.gen_img()
         self.itemconfig(self.image_canvas, image=self.img)
 
-    def start_animate(self):
+    def start_animate(self, animation_speed):
         self.is_animate = True
+        self.animation_speed = animation_speed
         self.next_frame()
 
     def next_frame(self):
         if self.is_animate and not self.animation_state.is_finish:
             self.animation_state.dfs()
             self.update_img()
-            self.after(250, self.next_frame)
+            self.after(self.animation_speed, self.next_frame)
             self.check_finish()
 
     def check_finish(self):
@@ -60,13 +64,14 @@ class AnimationScreen(tk.Canvas):
 
     def reset(self):
         self.animation_state.reset()
+        self.update_img()
 
 class AnimationState():
     def __init__(self, maze_manager):
         #maze_manager.maze = Eller()
         self.master_maze = maze_manager.maze
         self.init_maze = maze_manager.maze.get_maze()
-        self.curr_maze = self.init_maze
+        self.curr_maze = deepcopy(self.init_maze)
         self.height = maze_manager.maze.height
         self.width = maze_manager.maze.width
 
@@ -101,7 +106,8 @@ class AnimationState():
                 self.st.append((curr_y - 1, curr_x))
 
     def reset(self):
-        self.curr_maze = self.init_maze
+        self.is_finish = False
+        self.curr_maze = deepcopy(self.init_maze)
         self.curr_pos = (1,1)
         self.visited = [(1,1)]
         self.st = [(1, 1)]
@@ -112,6 +118,7 @@ class AnimationWindow(tk.Tk):
 
         # Constants
         self.DEFAULT_SIZE = (10, 10)
+        self.ANIMATION_SPEED = [2000, 1000, 100, 10, 1]
 
         # Header
         self.title("Maze Solver")
@@ -128,7 +135,7 @@ class AnimationWindow(tk.Tk):
             width=10, padx=2, pady=10)
         lbl1.pack()
 
-        self.maze_type = tk.StringVar(self, "eller")
+        self.maze_type = tk.StringVar(self, list(TYPES.keys())[0])
         type_buttons = {}
         for type in list(TYPES.keys()):
             button = tk.Radiobutton(self.right_frame, text=type, variable=self.maze_type,
@@ -151,6 +158,14 @@ class AnimationWindow(tk.Tk):
             font=("Arial", 12), bd=5)
         self.maze_height_entry.pack()
 
+        # Speed
+        tk.Label(self.right_frame, text = "Speed", font=("Arial", 15),
+            width=10, padx=2, pady=10).pack()
+        self.speed_slider = tk.Scale(self.right_frame, from_=1, to=len(self.ANIMATION_SPEED),
+            orient='horizontal')
+        self.speed_slider.set(3)
+        self.speed_slider.pack()
+
         # Initialize animation screen
         self.animation_screen = AnimationScreen(self, width=640, height=640)
         self.animation_screen.pack()
@@ -166,35 +181,48 @@ class AnimationWindow(tk.Tk):
             self.animation_screen.update_screen(maze_type=self.maze_type.get(),
                 maze_width=mw, maze_height=mh)
 
+            self.start_button.configure(state="normal")
+            self.reset_button.configure(state="disabled")
+
+
         # Generate button
         self.gen_button = tk.Button(self.right_frame, text="generate maze", pady=10,
             command=generate_maze)
         self.gen_button.pack()
 
         def start_animate():
-            self.animation_screen.start_animate()
-            #disable start button
+            self.animation_screen.start_animate(
+                animation_speed=self.ANIMATION_SPEED[self.speed_slider.get()-1])
+            self.start_button.configure(state="disabled")
+            self.stop_button.configure(state="normal")
+            self.gen_button.configure(state="disabled")
+            self.reset_button.configure(state="disabled")
 
         # Start button
         self.start_button = tk.Button(self.right_frame, text="start", pady=10, padx=20,
-            command=start_animate)
+            command=start_animate, state="disabled")
         self.start_button.pack()
 
         def stop_animate():
             self.animation_screen.stop_animate()
+            self.start_button.configure(state="normal")
+            self.stop_button.configure(state="disabled")
+            self.gen_button.configure(state="normal")
+            self.reset_button.configure(state="normal")
 
         # Stop button
         self.stop_button = tk.Button(self.right_frame, text="stop", pady=10, padx=20,
-            command=stop_animate)
+            command=stop_animate, state="disabled")
         self.stop_button.pack()
 
         def reset():
             self.animation_screen.reset()
+            self.start_button.configure(state="normal")
+            self.stop_button.configure(state="disabled")
 
         self.reset_button = tk.Button(self.right_frame, text="reset", pady=10, padx=20,
-            command=reset(), default="disabled")
-
-
+            command=reset, state="disabled")
+        self.reset_button.pack()
 
 def main():
     aw = AnimationWindow()
